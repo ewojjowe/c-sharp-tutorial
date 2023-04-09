94ed2a091f99232b357c55d9b87ef7e1521efaff
John Wilfred Montilde <wilfredjohnmontilde19@gmail.com>
2023-04-10 00:32:04 +0800

    Push Sample Namespace and comments

--- NamespaceAndComments/Program.cs
+++ NamespaceAndComments/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections;
 using NamespaceAndComments.Fruits;
+using NamespaceAndComments.Heroes.MobileLegends.Fighters;
+using NamespaceAndComments.Heroes.MobileLegends.Mages;
+
+// namespace alias
+using PinoyBisayaHero = NamespaceAndComments.Heroes.Pinoy.Bisaya;
 
 namespace NamespaceAndComments
 {
@@ -8,10 +13,31 @@ namespace NamespaceAndComments
     {
         static void Main(string[] args)
         {
+            #region -- Namespace Demo --
+
             Console.WriteLine("Hello World");
             var queue = new Queue();
             var banana = new Banana();
             var apple = new NamespaceAndComments.Fruits.Apple();
+
+            // Hero
+            var lapu2 = new LapuLapu();
+            var valir = new Valir();
+
+            var lapuLapu = new PinoyBisayaHero.LapuLapu();
+
+            #endregion
+
+            #region -- Comments Demo --
+
+            // this is a single line comment
+
+            /*
+             * this is a multiple line comment 
+             * format
+            */
+
+            #endregion
         }
     }
 }
